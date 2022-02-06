95be5f93a760d4ba4b8b4bb661137d006c28726d
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2022-02-05 19:14:59 -0800

    Added Client side and server side files

---
+++ client/src/js/install.js
@@ -0,0 +1,3 @@
+const butInstall = document.getElementById('buttonInstall');
+
+
---
+++ client/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      
+    ],
+
+    module: {
+      rules: [
+        
+      ],
+    },
+  };
+};
