02a9727eb642d6fff78683649d9a5fd8b938aa33
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2022-02-06 22:51:48 -0800

    Removed the bug from database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,17 +12,17 @@ const initdb = async () =>
     },
   });
 // Method that accepts some content and adds it to the database
-export const putDb = async (content) => {
+export const putDb = async (id, content) => {
   console.log('Database is updated!');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: 1, value: content });
+  const request = store.put({ id, content });
 
   const result = await request;
   console.log('Data successfully saved to the database', result);
   
-}
+};
 
 // Method that gets all the content from the database
 export const getDb = async () => {
@@ -34,7 +34,6 @@ export const getDb = async () => {
 
   const result = await request;
   console.log('Data from database:', result);
-  return result;
 }
 
 initdb();
